a74e4f6fc58ea747ce6f016a698bb5d274cd10aa
Milan He <179525809+Milanhe92@users.noreply.github.com>
2025-08-05 15:57:04 +0200

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,3 @@
- import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
@@ -13,18 +12,7 @@ const nextConfig = {
   transpilePackages: [
     '@melektron/ai-core',
     '@melektron/ton-utils'
-  ],
-  webpack:    config.resolve.alias = {
-     ...config.resolve.alias,
-     '@/components': path.resolve(__dirname, 'src/components'),
-     '@/components/licenses': path.resolve(__dirname, 'src/components/licenses'), 
-  (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      '@melektron/quantum-core': false
-    };
-    return config;
-  }
-};
+  ], 
+ };
 
 export default nextConfig;
