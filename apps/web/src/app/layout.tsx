71afb8e0ed668c584ec562b1392091942e993800
Milan He <179525809+Milanhe92@users.noreply.github.com>
2025-08-06 02:16:20 +0200

    Update layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import TonProvider from '@/providers/TonProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,12 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TonConnectUIProvider 
-          manifestUrl="https://melektron.ai/tonconnect-manifest.json"
-          uiPreferences={{ theme: 'dark' }}
-        >
+        <TonProvider>
           {children}
-        </TonConnectUIProvider>
+        </TonProvider>
       </body>
     </html>
   );
