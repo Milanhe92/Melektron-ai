5bd25d0926174ba63e0f95911a49058cd5524630
Milan He <milanhe92@gmail.com>
2025-08-23 19:53:07 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@melektron/quantum-core'],
+  webpack: (config) => {
+    config.resolve.alias = {
+  ...config.resolve.alias,
+      '@melektron/quantum-core': path.resolve(__dirname, '../../packages/quantum-core'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
