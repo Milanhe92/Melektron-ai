335cc0465579976630791b84c721df560068395b
Milan He <179525809+Milanhe92@users.noreply.github.com>
2025-08-23 17:51:51 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   transpilePackages: ['@melektron/quantum-core'],
   webpack: (config, { isServer }) => {
     config.resolve.alias = {
-     ...config.resolve.alias,
+    ...config.resolve.alias,
       '@melektron/quantum-core': path.resolve(__dirname, '../../packages/quantum-core'),
     };
     return config;
