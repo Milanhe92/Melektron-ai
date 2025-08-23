bb23501a529a857558c2190943f54841bc7faa92
Milan He <179525809+Milanhe92@users.noreply.github.com>
2025-08-23 05:40:22 +0200

    Update QuantumBlockchainBridge.ts

--- packages/quantum-core/src/lib/QuantumBlockchainBridge.ts
+++ packages/quantum-core/src/lib/QuantumBlockchainBridge.ts
@@ -1,19 +1,13 @@
-import { TonClient } from '@ton/core';
-
 export class QuantumBlockchainBridge {
-  private tonClient: TonClient;
-
-  constructor(client: TonClient) {
-    this.tonClient = client;
+  constructor() {
+    // Osnovni konstruktor
   }
 
   async sendQuantumState(state: any): Promise<void> {
-    // Implementacija slanja kvantnog stanja na blockchain
     console.log('Sending quantum state to blockchain...');
   }
 
   async receiveQuantumState(address: string): Promise<any> {
-    // Implementacija primanja kvantnog stanja sa blockchaina
     console.log('Receiving quantum state from blockchain...');
     return {};
   }
