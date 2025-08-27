dfc703449bd02d8915f4e919438ecfde4d999978
Milan He <milanhe92@gmail.com>
2025-08-28 01:01:25 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@melektron/quantum-core'],
+  transpilePackages: [
+    '@melektron/quantum-core',
+    '@ton/core',
+    '@ton/crypto',
+    '@ton/ton'
+  ],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer')
+    };
+    return config;
+  }
 };
 
 module.exports = nextConfig;
