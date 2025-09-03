faf8caba0f0015f884e1260eaa9480069af0c354
Milan He <milanhe92@gmail.com>
2025-09-03 06:14:18 +0200

    Create compiler.ts

---
+++ packages/ton-client/src/compiler.ts
@@ -0,0 +1,6 @@
+// packages/ton-client/src/compiler.ts
+export async function compile(): Promise<string> {
+  // Implementacija kompajlera za TON smart contract
+  // Ovo je primer - zamenite stvarnom implementacijom
+  return 'compiled_code_base64_here';
+}
