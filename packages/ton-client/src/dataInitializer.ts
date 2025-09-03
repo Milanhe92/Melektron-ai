2062e009366e80b30ce1eab336f0f01950f9d897
Milan He <milanhe92@gmail.com>
2025-09-03 06:15:40 +0200

    Create dataInitializer.ts

---
+++ packages/ton-client/src/dataInitializer.ts
@@ -0,0 +1,6 @@
+// packages/ton-client/src/dataInitializer.ts
+export async function initData(): Promise<any> {
+  // Implementacija inicijalizacije podataka
+  // Ovo je primer - zamenite stvarnom implementacijom
+  return { initialized: true };
+}
